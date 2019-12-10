ddb7978b3dfe0b50325e3ad5f495511f5653497a
itzfyp <itz.fyp@gmail.com>
2019-12-10 18:15:53 +0530

    order page added

--- routes/shop.js
+++ routes/shop.js
@@ -11,6 +11,8 @@ routes.get('/products', shopController.getProducts);
 
 routes.get('/cart', shopController.getCart);
 
+routes.get('/orders', shopController.getOrders);
+
 routes.get('/checkout', shopController.getCheckout);
 
 module.exports = routes;
