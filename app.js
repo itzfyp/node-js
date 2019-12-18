cbaaea77bb3206867a28f5b1b843308dd3230dfc
itzfyp <itz.fyp@gmail.com>
2019-12-18 15:52:40 +0530

    mongoDB added

--- app.js
+++ app.js
@@ -2,17 +2,13 @@ const path = require('path');
 const express = require('express');
 const bodyParser = require('body-parser');
 
-const adminRoutes = require('./routes/admin');
-const shopRoutes = require('./routes/shop');
-const errorController = require('./controllers/errors');
-const sequelize = require('./util/database');
 
-const Product = require('./models/product');
-const User = require('./models/user');
-const Cart = require('./models/cart');
-const CartItem = require('./models/cart-item');
-const Order = require('./models/order');
-const OrderItem = require('./models/order-item');
+const mongoConnect = require('./util/database');
+
+/* const adminRoutes = require('./routes/admin');
+const shopRoutes = require('./routes/shop'); */
+const errorController = require('./controllers/errors');
+
 
 const app = express();
 
@@ -25,7 +21,7 @@ app.set('views', 'views');
 // Allows to access static files from given folder
 const publicFolderAccess = express.static(path.join(__dirname, 'public'));
 
-app.use((req, res, next) => {
+/* app.use((req, res, next) => {
   User.findByPk(1)
     .then(user => {
       req.user = user;
@@ -34,45 +30,23 @@ app.use((req, res, next) => {
     .catch(err => {
       console.log('Error @App : User Registering');
     })
-});
+}); */
 app.use(publicFolderAccess);
 
-app.use('/admin', adminRoutes);
-app.use(shopRoutes);
-
-
+/* app.use('/admin', adminRoutes);
+app.use(shopRoutes); */
 app.use(errorController.get404);
 
-Product.belongsTo(User, { constraints: true, onDelete: 'CASCADE' });
-User.hasMany(Product);
-User.hasOne(Cart);
-Cart.belongsTo(User);
-Cart.belongsToMany(Product, { through: CartItem });
-Product.belongsToMany(Cart, { through: CartItem });
-Order.belongsTo(User);
-User.hasMany(Order);
-Order.belongsToMany(Product, { through: OrderItem });
+
+mongoConnect(client => {
+  console.log('client', client);
+
+  // App Server starts here
+  app.listen(3000)
+});
 
 
-sequelize
-  // .sync({ force: true })
-  .sync()
-  .then(() => User.findByPk(1))
-  .then(user => {
-    if (!user)
-      return User.create({ name: 'Jay', email: 'test@test.com' });
-    return Promise.resolve(user);
-  })
-  .then(user => {
-    return user.createCart();
-  })
-  .then(() => {
-    // App Server starts here
-    app.listen(3000)
-  })
-  .catch(err => {
-    console.log('sequelize Error', err);
-  });
+
 
 
 
--- util/database.js
+++ util/database.js
@@ -1,13 +1,46 @@
-const Sequelize = require('sequelize');
+// const mongodb = require('mongodb');
 
-const sequelize = new Sequelize(
-  'node-complete',
-  'root',
-  'akilan@J23',
-  {
-    dialect: 'mysql',
-    host: 'localhost'
-  }
-);
+// const MongoClient = mongodb.MongoClient;
 
-module.exports = sequelize;
+// const dbSrc = 'mongodb+srv://node-complete:node-complete@cluster0-ubk2k.mongodb.net/test?retryWrites=true';
+
+// const mongoConnect = cb => {
+//   MongoClient.connect(dbSrc, { useUnifiedTopology: true })
+//     .then(client => {
+//       console.log('Mongo connected.');
+//       cb(client);
+//     })
+//     .catch(err => {
+//       console.log('Error @ mongo db connect', err);
+//     });
+// };
+
+
+
+
+const MongoClient = require('mongodb').MongoClient;
+
+
+
+const mongoConnect = cb => {
+  console.log('calling mongoConnect...');
+  const uri = "mongodb+srv://node-complete:node-complete@cluster0-ubk2k.mongodb.net/test?retryWrites=true&w=majority";
+  const client = new MongoClient(uri, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+  });
+  client.connect()
+    .then(response => {
+      console.log('Mongo connected.');
+      cb(response);
+    })
+    .catch(err => {
+      const collection = client.db("test").collection("devices");
+      // perform actions on the collection object
+      client.close();
+    });
+
+};
+
+
+module.exports = mongoConnect;
