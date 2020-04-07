1c784c5f056d508b1e0aac3707225299bb626ebf
itzfyp <itzfyp@users.noreply.github.com>
2020-04-07 08:58:23 +0530

    singup functionality integrated with DB

--- app.js
+++ app.js
@@ -4,17 +4,17 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
 const session = require("express-session");
-const MongoDBstore = require("connect-mongodb-session")(session);
+const MongoDBStore = require("connect-mongodb-session")(session);
 
 const errorController = require("./controllers/error");
 const User = require("./models/user");
 
-const MONGO_DB_URI =
+const MONGODB_URI =
   "mongodb+srv://node-complete:node-complete@cluster0-ubk2k.mongodb.net/shop";
 
 const app = express();
-const store = new MongoDBstore({
-  uri: MONGO_DB_URI,
+const store = new MongoDBStore({
+  uri: MONGODB_URI,
   collection: "sessions",
 });
 
@@ -32,19 +32,20 @@ app.use(
     secret: "my secret",
     resave: false,
     saveUninitialized: false,
-    store,
+    store: store,
   })
 );
 
 app.use((req, res, next) => {
-  if (!req.session.user) return next();
-
+  if (!req.session.user) {
+    return next();
+  }
   User.findById(req.session.user._id)
     .then((user) => {
       req.user = user;
       next();
     })
-    .catch((err) => console.log("error in app user set"));
+    .catch((err) => console.log(err));
 });
 
 app.use("/admin", adminRoutes);
@@ -54,21 +55,8 @@ app.use(authRoutes);
 app.use(errorController.get404);
 
 mongoose
-  .connect(MONGO_DB_URI)
+  .connect(MONGODB_URI)
   .then((result) => {
-    console.log("Mongo Connected...");
-    User.findOne().then((user) => {
-      if (!user) {
-        const user = new User({
-          name: "Jay",
-          email: "test@test.com",
-          cart: {
-            items: [],
-          },
-        });
-        user.save();
-      }
-    });
     app.listen(3000);
   })
   .catch((err) => {
